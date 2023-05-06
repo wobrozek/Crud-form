490a51a26c3db650a792cf7201c618ea72b8176d
Wojciech Brożek <wobrozek@gmail.com>
2023-05-06 18:06:42 +0200

    add workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      'react': 'https://cdn.skypack.dev/react@18.2.0',
-      'react-dom': 'https://cdn.skypack.dev/react-dom@18.2.0'
-    }
-  },
+  // resolve: {
+  //   alias: {
+  //     'react': 'https://cdn.skypack.dev/react@18.2.0',
+  //     'react-dom': 'https://cdn.skypack.dev/react-dom@18.2.0'
+  //   }
+  // },
+  base: "/Crud-form/",
   plugins: [
     react()
   ],
