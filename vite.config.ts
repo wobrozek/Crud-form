5df4f94ff4ef3ada83240259a457567b8ca733f0
Wojciech Brożek <wobrozek@gmail.com>
2023-05-06 18:43:02 +0200

    port

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
   resolve: {
     alias: {
       'react': 'https://cdn.skypack.dev/react@18.2.0',
       'react-dom': 'https://cdn.skypack.dev/react-dom@18.2.0'
     }
   },
+  plugins: [
+    react()
+  ]
   // base: "/Crud-form/",
 
 })
