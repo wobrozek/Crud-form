e9a0e0d225e6ab08d46a663491e12181224413ce
Wojciech Brożek <wobrozek@gmail.com>
2023-05-06 15:52:32 +0200

    add serve

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 8081
-  },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
 })
