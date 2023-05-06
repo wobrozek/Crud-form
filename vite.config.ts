48accd95bfc69b13ed0e38c43fa0f8a63c8838b5
Wojciech Brożek <wobrozek@gmail.com>
2023-05-06 17:32:18 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
+  resolve: {
+    alias: {
+      'react': 'https://lf-eden-npm-cdn.bcystatic.com/obj/eden-npm-cdn/v1/dev/react/17.0.2/37d9c5c2.js',
+      'react-dom': 'https://lf-eden-npm-cdn.bcystatic.com/obj/eden-npm-cdn/v1/dev/react-dom/17.0.2/93d15a88.js'
+    }
   },
+  plugins: [
+    react()
+  ],
 })
