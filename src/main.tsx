74626d591336ba13b5525d7ddecafaeb891c67da
Wojciech Brożek <wobrozek@gmail.com>
2023-05-07 23:45:12 +0200

    add redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 import { Box } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading="loading.." persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore  } from '@reduxjs/toolkit';
 import languagesRedcuer from './languages';
-import formSlice from './form';
+import storage from 'redux-persist/lib/storage';
+import formReducer from './form';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  version:1,
+  storage,
+}
+
+const rootReducer = combineReducers({ 
+  languages:languagesRedcuer,
+  form:formReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    "languages":languagesRedcuer,
-    'form':formSlice,
-  },
+  reducer:persistedReducer,
+  middleware: getDefaultMiddleware =>
+  getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 });
 
+export const persistor =persistStore(store);
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
