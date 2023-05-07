f39498e12269ed21d5315b9e2e929317afc23a6b
Wojciech Brożek <wobrozek@gmail.com>
2023-05-07 12:16:02 +0200

    rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,19 @@ export default defineConfig({
       'react': 'https://cdn.skypack.dev/pin/react@v17.0.1-yH0aYV1FOvoIPeKBbHxg/mode=imports,min/optimized/react.js',
       'react-dom': 'https://cdn.skypack.dev/pin/react-dom@v17.0.1-oZ1BXZ5opQ1DbTh7nu9r/mode=imports,min/optimized/react-dom.js'
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+})
   // base: "/Crud-form/",
 
 })
