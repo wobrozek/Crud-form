0b8b199cc84b56e9ee47ea9bca8ec58d771a911d
Wojciech Brożek <wobrozek@gmail.com>
2023-05-07 12:22:22 +0200

    workflow config

--- vite.config.ts
+++ vite.config.ts
@@ -6,23 +6,23 @@ export default defineConfig({
   plugins: [
     react()
   ],
-  resolve: {
-    alias: {
-      'react': 'https://cdn.skypack.dev/pin/react@v17.0.1-yH0aYV1FOvoIPeKBbHxg/mode=imports,min/optimized/react.js',
-      'react-dom': 'https://cdn.skypack.dev/pin/react-dom@v17.0.1-oZ1BXZ5opQ1DbTh7nu9r/mode=imports,min/optimized/react-dom.js'
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
-  // base: "/Crud-form/",
+  // resolve: {
+  //   alias: {
+  //     'react': 'https://cdn.skypack.dev/pin/react@v17.0.1-yH0aYV1FOvoIPeKBbHxg/mode=imports,min/optimized/react.js',
+  //     'react-dom': 'https://cdn.skypack.dev/pin/react-dom@v17.0.1-oZ1BXZ5opQ1DbTh7nu9r/mode=imports,min/optimized/react-dom.js'
+  //   }
+  // },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["react", "react-dom"],
+  //     output: {
+  //       globals: {
+  //         react: "React",
+  //         "react-dom": "ReactDOM",
+  //       },
+  //     },
+  //   },
+  // },
+  base: "/Crud-form/",
 
 })
